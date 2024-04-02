37acaf524aaf85eba77c33de36f12ce4ccb89205
Nikhil-Jaiz <98412087+Nikhil-Jaiz@users.noreply.github.com>
2024-04-03 01:08:08 +0530

    Add files via upload

---
+++ dialog_box.js
@@ -0,0 +1,14 @@
+alert("Are you sure");
+const Age=confirm("Are you 18+");
+const n=prompt("Enter your age");
+console.log(n);
+if(Age) {
+    console.log("Yes");}
+
+    else console.log("No");
+
+
+//there are three types of dialog boxes in JS which are used to show POP-UPS in JS
+//these are alert prompt and confirm
+//alert function return undefined value ,prompt is used to take user input
+//confirm return bool value
---
+++ function.js
@@ -0,0 +1,10 @@
+function sum(a,b)  //function definition parameters
+{
+    return a+b;
+}
+// syntax of function in JS
+// function name_of_function(parameters)
+
+let a=10,b=13;
+let result=sum(a,b);  //function calling arguments
+console.log(result);
