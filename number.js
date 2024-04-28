b392014a6b032ea3228950d2a21ed8b75e9e9640
Nikhil-Jaiz <nikhiljaiswal20@gmail.com>
2024-04-28 23:27:25 +0530

    Update

---
+++ number.js
@@ -0,0 +1,15 @@
+const num=400;
+console.log(num);
+console.log(typeof(num));
+
+
+const balance=new Number(100);
+console.log(balance);
+console.log(typeof(balance));
+
+
+
+const hundered= Number(100000);
+console.log(hundered.toLocaleString());
+
+console.log(hundered.toString().length);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+
+const hundered= Number(100000);
+console.log(hundered.toLocaleString());
+
+console.log(hundered.toString().length);
