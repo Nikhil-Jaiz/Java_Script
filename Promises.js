1ed79116736bdd0272894ccbe3f6604c1a963007
Nikhil-Jaiz <nikhiljaiswal20@gmail.com>
2024-07-12 02:28:58 +0530

    Update

--- Promises.js
+++ Promises.js
@@ -8,3 +8,20 @@
 // Pending: Initial state, neither fulfilled nor rejected.
 // Fulfilled: The operation completed successfully.
 // Rejected: The operation failed.
+
+// Promise ia a object and JS does not have concept of Pointer (relate with C++)
+let Promise1=new Promise ( function(resolve,reject)
+{
+    setTimeout(function()
+       {
+        console.log("Performing an Async Event");
+       },2000)
+
+       resolve();  //if we don't execute or call resolve then  .then won't be executed
+});
+Promise1.then(function()
+{
+    console.log("Async Event Completed");
+})
+
+//Imp Note is that .then is related with resolve  so first we have to call resolve function
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,8 +1,11 @@
-console.log("Start");
-
-//SetTimeOut takes handlers that is Function without name
-setTimeout(()=>
+let Promise1=new Promise ( function(resolve,reject)
 {
-  console.log("The TimeOut is An asynchronous operation that will be executed after 2000ms and this does not block the flow of the code and will be executed at the last after end ")
-},2000);
-console.log("End");
+    setTimeout(function()
+       {
+        console.log("Performing an Async Time");
+       },2000)
+});
+Promise1.then(function()
+{
+    console.log("Async Event Completed");
+})
